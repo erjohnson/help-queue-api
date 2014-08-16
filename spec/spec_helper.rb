75707961e938141f0679511665f06b46d0f61882
Eric Johnson <erjohnson.pdx@gmail.com>
2014-08-16 00:56:15 +0000

    Update spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'rspec'
+require 'rack/test'
+require 'pg'
+require 'active_record'
+require 'pry'
+require 'shoulda/matchers'
+require 'ticket'
+require 'project'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))["test"])
+
+RSpec.configure do |config|
+  config.formatter = 'doc'
+  config.include Rack::Test::Methods
+  config.after(:each) do
+    Ticket.all.each { |ticket| ticket.destroy }
+    Project.all.each { |project| project.destroy }
+  end
+end
